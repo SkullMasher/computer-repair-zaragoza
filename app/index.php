1fb878400a7cd747d94eb9f30c47437be2482d76
Skullmasher <skullm4sher@gmail.com>
2018-11-21 01:00:20 +0100

    :skull: I don't even know anymore just need a savepoint

--- app/index.php
+++ app/index.php
@@ -8,36 +8,37 @@
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-  <div class="box-center-fullscreen">
-    <div class="card">
-      <header class="tile-background">
-        <div class="hero-content">
-          <!-- <picture class="logo"><?php // echo file_get_contents('img/logo.svg') ?></picture> -->
-          <h1 class="main-title">¡Reparación de ordenadores!</h1>
-          <p class="main-text">Si tu ordenador va muy lento, crees que puede estar roto, o estás pensando en instalar o conseguir algún programa ¡Llámame! Arreglaré tu ordenador, sea lo sea lo que le pase, por un módico precio.</p>
+  <div class="page-head">
+    <header class="page-head__background">
+      <div class="hero-content">
+        <picture class="logo"><?php echo file_get_contents('img/logo.svg') ?></picture>
+        <div class="item-fluid">
+          <h1 class="page-head__title">¡Reparación de ordenadores!</h1>
+          <p class="page-head__tagline">Si tu ordenador va muy lento, crees que puede estar roto, o estás pensando en instalar o conseguir algún programa ¡Llámame! Arreglaré tu ordenador, sea lo sea lo que le pase, por un módico precio.</p>
         </div>
-      </header>
-      <footer class="flex-center">
-        <div class="contact">
-          <div class="contact__qrcode">
-            <?php echo file_get_contents('img/qrcode.svg') ?>
+      </div>
+    </header>
+    <footer class="flex-center">
+      <div class="contact">
+        <div class="contact__qrcode">
+          <?php echo file_get_contents('img/qrcode.svg') ?>
+        </div>
+        <div class="nevermore">
+          <div>
+            <a class="contact__btn contact__phone btn--primary" href="tel:+34645397709">
+              <picture class="contact__picto"><?php echo file_get_contents('img/phone.svg') ?></picture>
+              <p>645 397 709</p>
+            </a>
           </div>
-          <div class="nevermore">
-            <div>
-              <a class="contact__btn contact__phone btn--primary" href="tel:+34645397709">
-                <picture class="contact__picto"><?php echo file_get_contents('img/phone.svg') ?></picture>
-                <p>645 397 709</p>
-              </a>
-            </div>
-            <div>
-              <a class="contact__btn contact__mail btn--success" href="mailto:florian@skullmasher.io">
-                <picture class="contact__picto"><?php echo file_get_contents('img/email.svg') ?></picture>
-                <p>florian@skullmasher.io</p>
-              </a>
-            </div>
+          <div>
+            <a class="contact__btn contact__mail btn--success" href="mailto:florian@skullmasher.io">
+              <picture class="contact__picto"><?php echo file_get_contents('img/email.svg') ?></picture>
+              <p>florian@skullmasher.io</p>
+            </a>
           </div>
         </div>
-      </footer>
+      </div>
+    </footer>
       <!-- <div class="grab-info">
         <p>+33625970561</p>
         <p>florian@skullmasher.io</p>
@@ -58,6 +59,5 @@
         <p>+33625970561</p>
       </div> -->
     </div>
-  </div>
-</body>
-</html>
+  </body>
+  </html>
